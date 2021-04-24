0598e8efc639dd22bc48c8540d4185f61e192972
anandantony <anandantony.7599@gmail.com>
2021-04-24 18:50:21 +0530

    removed fade

--- public/index.js
+++ public/index.js
@@ -103,23 +103,3 @@ function render() {
     renderer.clear();
     renderer.render(scene, camera);
 };
-
-///
-
-const faders = document.querySelectorAll('.fade-in');
-const appearOptions = {
-    threshold: 1
-};
-const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        }
-        entry.target.classList.add('appear');
-        appearOnScroll.unobserve('entry.target');
-    })
-}, appearOptions);
-
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-});
